d526b69ab228771c8a5722a30c4136f65c3e1cf7
xinhe <xinhe9715@gmail.com>
2018-03-02 20:53:09 +0800

    abbrasion and output image
    Now, add_abrasion() availabble.
    Deleted output part in add_XXX()'s, uses a new function to output images write_image().

--- preprocess/main.py
+++ preprocess/main.py
@@ -1,12 +1,18 @@
 import numpy as np
 import cv2 as cv
+import os
 import add_defect as ad
 
-for n in range(1,100):
+for n in range(1,10):
 	m = int(np.random.uniform(1,3369))
 	print(n,m)
 	
 	df = ad.AddDefect(m,n)
-	#df.add_dot()
-	#df.add_spot()
+	df.add_abrasion()
+	df.write_image("./generated_images/abrasion/abrasion_"+str(n)+".png")
+"""
+	df.add_spot()
 	df.add_cut()
+	
+	df.write_image("./generated_images/defect/df_"+str(n)+".png")
+"""
